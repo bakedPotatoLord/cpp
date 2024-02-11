d5bf6e2bf7d1a2486c36b378458d43d257edd8ff
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2024-02-11 20:13:31 +0000

    fix and finish algorithm

--- CSC1061_jhamm_Challenge5/CSC1061_kgeise_Challenge1/main.cpp
+++ CSC1061_jhamm_Challenge5/CSC1061_kgeise_Challenge1/main.cpp
@@ -5,8 +5,6 @@
 
 using namespace std;
 
-
-
 int main()
 {
 
@@ -16,55 +14,55 @@ int main()
         {1,0,0,0,0,0,0},
         {0,0,1,0,1,0,1}
     };
+
+    printMatrix(vec);
+
+    // point p =*new point {1,1};
+    // auto pts = getTouching(p,10,10);
+
+    // for(auto pt : pts){
+    //     cout << pt->i << ' ' << pt->j << endl;
+    // }
+
+
     //calculate number of islands
-    cout << numIslands(vec);
+    int nislands = numIslands(vec);
+
+    cout <<"num islands: " << nislands;
 
     return 0;
 };
 
 int numIslands(vector<vector<bool>> mx){
-
+    int islands = 0;
+    //find height and length
+    const int height = mx.size();
+    const int length = mx[0].size();
     
-    
-    cout << "matrix:" <<endl;
-    printMatrix(mx);
-
-    cout << "visited: " << endl;
-    printMatrix(visited);
-
-    int numIslands = 0;
-
-    while(!possibleStack.empty()){
-        auto top = possibleStack.top();
-        possibleStack.pop();
-        int i = top->i;
-        int j = top->j;
-
-
-
-        if(visited[i][j]){
-            //don't do anything
-        }else{
-            visited[i][j] = 1;
-
-
-            if(mx[i][j]){
-                //if there is an island
+    for(int i =0; i< height;i++){
+        for(int j = 0 ; j<length;j++){ //iterating over all possible points of the matrix
+            if(mx[i][j]){ // if looking at island for first time
+                islands++;
+            
+                stack<point*> island ; //create stack for visiting the island
                 
+                island.push(new point{i,j}); // push first point for starting
 
-                auto touching = getTouching(point(i,j))
+                while(!island.empty()){ //for all ajacent island points
+                    point p = *island.top(); //derefrence and pop the top of the stack
+                    island.pop();
 
-                while()
-
+                    if(mx[p.i][p.j]){ //if looking at island
+                        mx[p.i][p.j] = 0; //set it to 0
+                        for (auto v : getTouching(p,height,length)){
+                            island.push(v); // push touching elements
+                        }
+                    }
+                }
             }
         }
     }
-
-    cout << "visited: " << endl;
-    printMatrix(visited);
-    
-
-    return 0;
+    return islands;
 };
 
 
@@ -75,7 +73,7 @@ vector<point*> getTouching(const point p,int height,int width){
     if(p.i >0) points.push_back(new point{p.i-1,p.j});
     if(p.i < height-1) points.push_back(new point{p.i+1,p.j});
 
-    (p.j >0) points.push_back(new point{p.i,p.j-1});
+    if(p.j >0) points.push_back(new point{p.i,p.j-1});
     if(p.j < width-1) points.push_back(new point{p.i,p.j+1});
 
     return points;
--- CSC1061_jhamm_Challenge5/CSC1061_kgeise_Challenge1/main.h
+++ CSC1061_jhamm_Challenge5/CSC1061_kgeise_Challenge1/main.h
@@ -10,5 +10,6 @@ struct point{
 
 int main();
 int numIslands(std::vector<std::vector<bool>> );
+std::vector<point*> getTouching(const point p, const int height, const int length);
 template<typename T>
 void printMatrix(const std::vector<std::vector<T>> mx);
