43aad39559c703cfb2e4cda29a3db64644feb620
bakedPotatoLord <jeffy9745@gmail.com>
2024-02-13 08:38:28 -0700

    reformat

--- CSC1061_jhamm_Challenge5/CSC1061_kgeise_Challenge1/main.cpp
+++ CSC1061_jhamm_Challenge5/CSC1061_kgeise_Challenge1/main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <vector>
 #include <stack>
@@ -8,78 +9,84 @@ using namespace std;
 
 int main()
 {
-
+    // Define test cases
     vector<vector<vector<bool>>> tests{
-        {
-            {0,0,1,1,0,1,0},
-            {1,0,1,0,0,1,0},
-            {1,0,0,0,0,0,0},
-            {0,0,1,0,1,0,1}    
-        },
-        {
-            {1,1},
-            {1,1}
-        },
-        {
-            {1,0},
-            {0,1}
-        },
-        {
-            {1,0,0,0,1},
-            {0,0,0,0,0},
-            {0,0,1,0,0},
-            {0,0,0,0,0},
-            {1,0,0,0,1}
-        },
-        {
-            {0}
-        },
-        {
-            {1}
-        },
-        {
-            {0,1,1,1,0,1,0,1,0,0,1,1}
-        }
-    };
+        {{0, 0, 1, 1, 0, 1, 0},
+         {1, 0, 1, 0, 0, 1, 0},
+         {1, 0, 0, 0, 0, 0, 0},
+         {0, 0, 1, 0, 1, 0, 1}},
+        {{1, 1},
+         {1, 1}},
+        {{1, 0},
+         {0, 1}},
+        {{1, 0, 0, 0, 1},
+         {0, 0, 0, 0, 0},
+         {0, 0, 1, 0, 0},
+         {0, 0, 0, 0, 0},
+         {1, 0, 0, 0, 1}},
+        {{0}},
+        {{1}},
+        {{0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1}}};
 
-    vector<int> answers = { 6,1,2,5,0,1,4};
+    // Define expected answers
+    vector<int> answers = {6, 1, 2, 5, 0, 1, 4};
 
-    assert(tests.size() == answers.size()); // make sure tests are set up correctly
+    // Ensure the size of tests and answers match
+    assert(tests.size() == answers.size());
 
-   for(int i  = 0 ; i<tests.size();i++){
-    printMatrix(tests[i]);
-    auto answer = numIslands(tests[i]);
-    cout << "algorithm answer: " << answer << endl;
-    cout << "answer "  << ((answers[i] == answer)? "correct👍": "incorrect👎") << endl <<endl;
-    assert(answers[i] == answer);
-   }
+    // Iterate over test cases
+    for (int i = 0; i < tests.size(); i++)
+    {
+        // Print the matrix for the current test case
+        printMatrix(tests[i]);
+        // Get the number of islands using the algorithm
+        auto answer = numIslands(tests[i]);
+        // Print the algorithm's answer
+        cout << "algorithm answer: " << answer << endl;
+        // Check if the answer matches the expected answer
+        cout << "answer " << ((answers[i] == answer) ? "correct" : "incorrect") << endl
+             << endl;
+        // Assert the correctness of the answer
+        assert(answers[i] == answer);
+    }
 
     return 0;
 };
 
-int numIslands(vector<vector<bool>> mx){
+// Function to count the number of islands
+int numIslands(vector<vector<bool>> mx)
+{
     int islands = 0;
-    //find height and length
+    // Find height and length of the matrix
     const int height = mx.size();
     const int length = mx[0].size();
-    
-    for(int i =0; i< height;i++){
-        for(int j = 0 ; j<length;j++){ //iterating over all possible points of the matrix
-            if(mx[i][j]){ // if looking at island for first time
-                islands++;
-            
-                stack<point*> island ; //create stack for visiting the island
-                
-                island.push(new point{i,j}); // push first point for starting
 
-                while(!island.empty()){ //for all ajacent island points
-                    point p = *island.top(); //derefrence and pop the top of the stack
+    // Iterate through each point in the matrix
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < length; j++)
+        {
+            // If the current point is part of an island
+            if (mx[i][j])
+            {
+                islands++; // Increment the island count
+
+                stack<point *> island;        // Create stack for visiting the island
+                island.push(new point{i, j}); // Push the first point for starting
+
+                // Traverse the island
+                while (!island.empty())
+                {
+                    point p = *island.top(); // Dereference and pop the top of the stack
                     island.pop();
 
-                    if(mx[p.i][p.j]){ //if looking at island
-                        mx[p.i][p.j] = 0; //set it to 0
-                        for (auto v : getTouching(p,height,length)){
-                            island.push(v); // push touching elements
+                    if (mx[p.i][p.j])
+                    {                     // If the current point is part of the island
+                        mx[p.i][p.j] = 0; // Mark it as visited
+                        // Add adjacent points to the stack
+                        for (auto v : getTouching(p, height, length))
+                        {
+                            island.push(v);
                         }
                     }
                 }
@@ -89,29 +96,34 @@ int numIslands(vector<vector<bool>> mx){
     return islands;
 };
 
+// Function to get adjacent points
+vector<point *> getTouching(const point p, int height, int width)
+{
+    vector<point *> points;
+    if (p.i > 0)
+        points.push_back(new point{p.i - 1, p.j});
+    if (p.i < height - 1)
+        points.push_back(new point{p.i + 1, p.j});
 
-
-
-vector<point*> getTouching(const point p,int height,int width){
-    vector<point*> points;
-    if(p.i >0) points.push_back(new point{p.i-1,p.j});
-    if(p.i < height-1) points.push_back(new point{p.i+1,p.j});
-
-    if(p.j >0) points.push_back(new point{p.i,p.j-1});
-    if(p.j < width-1) points.push_back(new point{p.i,p.j+1});
+    if (p.j > 0)
+        points.push_back(new point{p.i, p.j - 1});
+    if (p.j < width - 1)
+        points.push_back(new point{p.i, p.j + 1});
 
     return points;
 }
 
-template<typename T>
-void printMatrix(vector<vector<T>> mx){
-    for(const vector<bool>& vec :mx){
-       
-        for(const bool& b : vec){
+// Template function to print the matrix
+template <typename T>
+void printMatrix(vector<vector<T>> mx)
+{
+    for (const vector<bool> &vec : mx)
+    {
+        for (const bool &b : vec)
+        {
             cout << b << ' ';
         }
         cout << endl;
     }
     cout << endl;
 }
-
