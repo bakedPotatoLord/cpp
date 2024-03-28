6f53024ccddaf40281f978696b726174b70401f5
= <918780@dpsk12.net>
2024-03-27 18:08:49 -0600

    add chatgpt comments

--- CSC1061_jhamm_Challenge4/BinaryTree.h
+++ CSC1061_jhamm_Challenge4/BinaryTree.h
@@ -1,93 +1,142 @@
-#pragma once
 
-#include "EmployeeInfo.h"
+#pragma once // Preprocessor directive to ensure that the header file is
+             // included only once
 
+#include "EmployeeInfo.h" // Include the header file that contains the EmployeeInfo class
+
+// Structure for a tree node containing data of type T
 template <class T> struct treeNode {
-  T data;
-  struct treeNode *left;
-  struct treeNode *right;
+  T data;                 // Data stored in the node
+  struct treeNode *left;  // Pointer to the left child node
+  struct treeNode *right; // Pointer to the right child node
 };
 
+// Class for a binary tree with nodes containing data of type T
 template <class T> class BinaryTree {
 private:
-  treeNode<T> *root;
-  int count;
+  treeNode<T> *root; // Pointer to the root node of the binary tree
+  int count;         // Number of nodes in the binary tree
 
 public:
+  // Function to add a new node with data of type T to the binary tree
   void add(T data);
+
+  // Function to search for a node with a given ID in the binary tree
   T search(int id);
+
+  // Function to check if a node with a given ID exists in the binary tree
   bool hasID(int id);
+
+  // Function to check if the binary tree is empty
   bool isEmpty();
+
+  // Function to get the root node of the binary tree
   treeNode<T> getRoot();
+
+  // Constructor with parameter to initialize the binary tree with a root node
   BinaryTree(T root);
+
+  // Default constructor to initialize an empty binary tree
   BinaryTree();
 };
 
+// Implementation of the add function to add a new node to the binary tree
 template <class T> inline void BinaryTree<T>::add(T data) {
-  treeNode<T> *val = new treeNode<T>;
-  val->left = NULL;
-  val->right = NULL;
-  val->data = data;
-  treeNode<T> *parent = NULL;
-  if (isEmpty()) {
+  treeNode<T> *val = new treeNode<T>; // Create a new tree node with data
+  val->left = NULL;                   // Initialize left child pointer to null
+  val->right = NULL;                  // Initialize right child pointer to null
+  val->data = data;                   // Set the data of the new node
+  treeNode<T> *parent =
+      NULL; // Pointer to track the parent node during traversal
+
+  if (isEmpty()) { // If the binary tree is empty, set the new node as the root
     root = val;
   } else {
-    treeNode<T> *ptr = root;
+    treeNode<T> *ptr =
+        root; // Pointer to traverse the binary tree starting from the root
     while (ptr != NULL) {
-      parent = ptr;
+      parent = ptr; // Update the parent pointer
       if (data > ptr->data) {
-        ptr = ptr->right;
+        ptr = ptr->right; // Move to the right child if the new data is greater
+                          // than the current node's data
       } else if (data < ptr->data) {
-        ptr = ptr->left;
+        ptr = ptr->left; // Move to the left child if the new data is less than
+                         // the current node's data
       }
     }
+
+    // Insert the new node as the left or right child of the parent node based
+    // on the comparison result
     if (parent->data > data) {
       parent->left = val;
     } else if (parent->data < data) {
       parent->right = val;
     } else {
-      cout << "duplicates are not allowed";
+      cout << "duplicates are not allowed"; // Duplicate data is not allowed in
+                                            // the binary tree
     }
   }
 }
 
+// Implementation of the search function to find a node with a given ID in the
+// binary tree
 template <class T> inline T BinaryTree<T>::search(int id) {
-  treeNode<T> *n = root;
+  treeNode<T> *n = root; // Start the search from the root node
 
   while (n != NULL) {
     if (n->data > id) {
-      n = n->left;
+      n = n->left; // Move to the left child if the current node's data is
+                   // greater than the target ID
     } else if (n->data < id) {
-      n = n->right;
+      n = n->right; // Move to the right child if the current node's data is
+                    // less than the target ID
     } else {
-      return n->data;
+      return n->data; // Return the data of the node if the target ID is found
     }
   }
-  throw "no employee found";
+  throw "no employee found"; // Throw an exception if the target ID is not found
+                             // in the binary tree
 }
 
+// Implementation of the hasID function to check if a node with a given ID
+// exists in the binary tree
 template <class T> inline bool BinaryTree<T>::hasID(int id) {
-  treeNode<T> *n = root;
+  treeNode<T> *n = root; // Start the search from the root node
 
   while (n != NULL) {
     if (n->data > id) {
-      n = n->left;
+      n = n->left; // Move to the left child if the current node's data is
+                   // greater than the target ID
     } else if (n->data < id) {
-      n = n->right;
+      n = n->right; // Move to the right child if the current node's data is
+                    // less than the target ID
     } else {
-      return true;
+      return true; // Return true if the target ID is found in the binary tree
     }
   }
-  return false;
+  return false; // Return false if the target ID is not found in the binary tree
 }
 
-template <class T> inline treeNode<T> BinaryTree<T>::getRoot() { return *root; }
+// Implementation of the getRoot function to get the root node of the binary
+// tree
+template <class T> inline treeNode<T> BinaryTree<T>::getRoot() {
+  return *root; // Return the root node of the binary tree
+}
 
-template <class T> inline BinaryTree<T>::BinaryTree(T root) { add(T); }
+// Constructor implementation with a parameter to initialize the binary tree
+// with a root node
+template <class T> inline BinaryTree<T>::BinaryTree(T root) {
+  add(T); // Add the root node to the binary tree
+}
 
+// Default constructor implementation to initialize an empty binary tree
 template <class T> inline BinaryTree<T>::BinaryTree() {
-  root = NULL;
-  count = 0;
+  root = NULL; // Set the root pointer to null to indicate an empty binary tree
+  count = 0;   // Initialize the count of nodes to zero
 }
 
-template <class T> inline bool BinaryTree<T>::isEmpty() { return root == NULL; }
+// Implementation of the isEmpty function to check if the binary tree is empty
+template <class T> inline bool BinaryTree<T>::isEmpty() {
+  return root == NULL; // Return true if the root pointer is null, indicating an
+                       // empty binary tree
+}
--- CSC1061_jhamm_Challenge4/EmployeeInfo.cpp
+++ CSC1061_jhamm_Challenge4/EmployeeInfo.cpp
@@ -1,41 +1,56 @@
-#include "EmployeeInfo.h"
-#include <iostream>
+#include "EmployeeInfo.h" // Include the header file that contains the class declaration
+#include <iostream> // Include the standard input/output stream library
 
-using namespace std;
+using namespace std; // Using the standard namespace for cout and endl
 
+// Set the name of the employee
 void EmployeeInfo::setName(string newName) { name = newName; }
 
+// Get the name of the employee
 string EmployeeInfo::getName() { return name; }
 
+// Get the ID of the employee
 int EmployeeInfo::getID() { return id; }
 
+// Print the information of the employee
 void EmployeeInfo::print() {
   cout << "Employee #" << id << "\t" << name << endl;
 }
 
+// Overloaded equality operator to compare two EmployeeInfo objects
 bool EmployeeInfo::operator==(EmployeeInfo const &other) {
   return id == other.id;
 }
 
+// Overloaded greater than operator to compare two EmployeeInfo objects
 bool EmployeeInfo::operator>(EmployeeInfo const &other) {
   return id > other.id;
 }
 
+// Overloaded less than operator to compare two EmployeeInfo objects
 bool EmployeeInfo::operator<(EmployeeInfo const &other) {
   return id < other.id;
 }
 
+// Overloaded equality operator to compare an EmployeeInfo object with an
+// integer
 bool EmployeeInfo::operator==(int const &other) { return id == other; }
 
+// Overloaded greater than operator to compare an EmployeeInfo object with an
+// integer
 bool EmployeeInfo::operator>(int const &other) { return id > other; }
 
+// Overloaded less than operator to compare an EmployeeInfo object with an
+// integer
 bool EmployeeInfo::operator<(int const &other) { return id < other; }
 
+// Constructor with parameters to initialize name and id
 EmployeeInfo::EmployeeInfo(string name, int id) {
   this->name = name;
   this->id = id;
 }
 
+// Default constructor to initialize name and id with default values
 EmployeeInfo::EmployeeInfo() {
   name = "unnamed";
   id = -1;
